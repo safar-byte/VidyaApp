d64a3d76e0109c57ad58698179cd8be5d7c68786
shameemx <120473767+shameemx@users.noreply.github.com>
2022-12-28 20:53:34 +0530

    address completed

--- screens/Contact_sc.js
+++ screens/Contact_sc.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Text, View ,StyleSheet} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Contact_sc(){
-    return(
+export default function Contact_sc() {
+    return (
         <View style={styles.cont}>
-            <Text>contact</Text>
+            <Text style={styles.conta}>Contact Us</Text>
+            <Text style={styles.vast}>Vidya Academy of Science & Technology Technical Campus</Text>
+            <Text style={styles.add}>Address:</Text>
+            <Text style={styles.ad}>Malakkal P.O., Kilimanoor, Thiruvananthapuram - 695 602, Kerala, India</Text>
         </View>
     );
 }
@@ -13,6 +16,41 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#E5BA73',
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
+    },
+    conta: {
+        flex: 1,
+        fontSize: 25,
+        fontWeight: 'Bold',
+        color: 'brown',
+        paddingVertical: 20,
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    vast: {
+        flex: 1,
+        fontSize: 17,
+        fontWeight: 'bold',
+        color: 'brown',
+        position: 'absolute',
+        top: 75,
+        left: 10
+    },
+    add: {
+        flex: 1,
+        fontSize: 17,
+        fontWeight: 'semi bold',
+        color: 'brown',
+        position: 'absolute',
+        top: 130,
+        left: 10
+    },
+    ad: {
+        flex: 1,
+        fontSize: 13,
+        position: 'absolute',
+        top: 155,
+        left: 10
     }
- })
+})

01c9cad002b586c8028d65e0c5282adbb31fc608
shameemx <120473767+shameemx@users.noreply.github.com>
2022-12-29 15:34:49 +0530

    contact ui completed

--- screens/Contact_sc.js
+++ screens/Contact_sc.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Linking, Platform, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Linking, Platform, TouchableOpacity, Dimensions } from "react-native";
 import { Ionicons } from '@expo/vector-icons'
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function Contact_sc() {
 
@@ -17,104 +20,95 @@ export default function Contact_sc() {
         Linking.openURL(phoneNumber);
     };
     openEmail = () => {
-        Linking.openURL("mailto: mhdshameem321@gmail.com")
+        Linking.openURL("mailto: info@vidyatcklmr.ac.in")
     }
 
     return (
 
-        < View style={styles.cont} >
-            <Text style={styles.conta}>Contact Us</Text>
-            <Text style={styles.vast}>Vidya Academy of Science & Technology Technical Campus</Text>
-            <Text style={styles.add}>Address:</Text>
-            <Text style={styles.ad}>Malakkal P.O., Kilimanoor, Thiruvananthapuram - 695 602, Kerala, India</Text>
-            <Ionicons name="call-outline" size={20} style={styles.ga} />
-            <Text style={{ marginHorizontal: 15, bottom: 390, right: 130, fontSize: 17, fontWeight: 'bold' }}>Call</Text>
-            <TouchableOpacity onPress={this.makeCall} activeOpacity={0.7} style={styles.touchableButton} >
-                <Text style={styles.TextStyle}>9447540982</Text>
-            </TouchableOpacity>
-            <Ionicons name="mail-open-outline" size={20} style={styles.ma} />
-            <Text style={{ marginHorizontal: 15, bottom: 380, right: 130, fontSize: 17, fontWeight: 'bold' }}>Mail</Text>
-            <TouchableOpacity onPress={() => openEmail()}>
-                < Text style={styles.tex}>mhdshameem321@gmail.com</Text>
-            </TouchableOpacity></View >
+        < View style={styles.container} >
+            <View style={styles.tphf}>
+                <View style={styles.conta}>
+                    <Text style={{ fontSize: 25, fontWeight: 'bold', color: "brown" }}>Contact Us</Text>
+                </View>
+                <View style={styles.vast}>
+                    <Text style={styles.head}>Vidya Academy of Science & Technology </Text>
+                    <Text style={styles.head}>Technical Campus</Text>
+                </View >
+                <View style={{ marginStart: 10 }}>
+                    <Text style={styles.add}>Address:</Text>
+                    <Text style={{ fontSize: 13 }}>Malakkal P.O., Kilimanoor, Thiruvananthapuram - 695 602, Kerala, India</Text>
+                </View>
+            </View>
+            <View style={styles.btmhf}>
+                <View style={styles.ga}>
+                    <Ionicons name="call-outline" size={20} />
+                    <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Call</Text>
+                </View>
+                <TouchableOpacity onPress={makeCall} activeOpacity={0.7} style={styles.touchableButton} >
+                    <Text style={styles.TextStyle}>9447540982</Text>
+                </TouchableOpacity>
+                <View style={styles.ma}>
+                    <Ionicons name="mail-open-outline" size={20} />
+                    <Text style={{ fontSize: 15, fontWeight: 'bold' }}>Mail</Text>
+                </View>
+                <TouchableOpacity onPress={() => openEmail()}>
+                    < Text style={{
+                        color: 'blue',
+                        fontSize: 18
+                    }}>info@vidyatcklmr.ac.in</Text>
+                </TouchableOpacity></View >
+        </View >
 
     );
 }
 
 
 const styles = StyleSheet.create({
-    cont: {
-        flex: 1,
-        backgroundColor: '#E5BA73',
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    conta: {
-        flex: 1,
-        fontSize: 25,
-        fontWeight: 'Bold',
-        color: 'brown',
-        paddingVertical: 20,
-        justifyContent: "center",
-        alignItems: "center"
-
-    },
-    vast: {
-        flex: 1,
-        fontSize: 17,
-        fontWeight: 'bold',
-        color: 'brown',
-        position: 'absolute',
-        top: 75,
-        left: 10
-    },
-    add: {
-        flex: 1,
-        fontSize: 17,
-        fontWeight: 'semi bold',
-        color: 'brown',
-        position: 'absolute',
-        top: 130,
-        left: 10
-    },
-    ad: {
-        flex: 1,
-        fontSize: 13,
-        position: 'absolute',
-        top: 155,
-        left: 10
-    },
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 20,
+        backgroundColor: '#E5BA73',
+        //justifyContent: "center",
+        alignItems: "center",
+
     },
+    tphf: {
+        width: "100%",
+        height: "20%"
+    },
+    conta: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    vast: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    head: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: "brown"
+    },
+    add: {
+        paddingTop: 10,
+        fontSize: 18,
+        fontWeight: 'semi bold',
+        color: 'brown',
+    },
+    btmhf: {
+        padding: 10,
+        paddingTop: 20,
+        width: "100%"
+    },
+
     touchableButton: {
         width: '40%',
         padding: 2,
         backgroundColor: 'brown',
-        bottom: 380,
-        right: 90
     },
     TextStyle: {
         color: '#fff',
         fontSize: 18,
         textAlign: 'center',
-    },
-    ga: {
-        bottom: 368,
-        right: 160
-    },
-    ma: {
-        bottom: 358,
-        right: 160
-    },
-    tex: {
-        color: 'blue',
-        fontSize: 18,
-        bottom: 375,
-        right: 40
     }
 
 });

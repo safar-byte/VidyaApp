3ed3ae432634fb246dd7fe4c63ea4a91fb88438c
son7a <121379079+son7a@users.noreply.github.com>
2022-12-29 20:50:31 +0530

    page loaded

--- screens/Resource_sc.js
+++ screens/Resource_sc.js
@@ -1,18 +1,78 @@
-import React from "react";
-import { Text, View ,StyleSheet} from "react-native";
+import React,{useEffect,useRef,useState}  from "react";
+import { Text, View ,StyleSheet,ActivityIndicator,BackHandler,Platform,LogBox} from "react-native";
+import { WebView } from 'react-native-webview';
+import OfflineNotice from "../components/OfflineNotice";
+import NetInfo from "@react-native-community/netinfo";
+import SomethingWent from "../components/SomethingWent"; 
 
 export default function Resource_sc(){
-    return(
-        <View style={styles.cont}>
-            <Text>Resource</Text>
-        </View>
+    const webViewRef = useRef(null);
+const onAndroidBackPress = () => {
+  if (webViewRef.current) {
+    webViewRef.current.goBack();
+    return true; // prevent default behavior (exit app)
+  }
+  return false;
+};
+
+
+
+useEffect(() => {
+    if (Platform.OS === 'android') {
+        BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+        return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    };
+}
+}, []);
+const [isInternetReachable, setisInternetReachable] = useState(false);
+useEffect(()=>{
+    const unsubscribe = NetInfo.addEventListener(state => {
+        setisInternetReachable(state.isInternetReachable)
+    });
+    return () => unsubscribe();
+    
+},[])
+
+//remove all warn
+LogBox.ignoreAllLogs();
+ return(
+  <>      
+           <View style =  {{flex: 1}}>
+        {isInternetReachable ?   
+        <WebView
+        ref={webViewRef}
+        style={styles.container}
+        source={{ uri: 'https://www.vidyatcklmr.ac.in/page.php?page=92 '}}
+        startInLoadingState={true}
+       r
+        renderError={()=>(<SomethingWent/>)}
+        renderLoading={() => (
+          <ActivityIndicator
+          color="black"
+          size="large"
+          style={styles.flexContainer}
+          />
+          )}
+          />:<OfflineNotice/>
+          
+        }
+      
+</View>
+      </>
     );
 }
 const styles = StyleSheet.create({
-    cont: {
+    container: {
         flex: 1,
         backgroundColor: '#E5BA73',
         justifyContent: "center",
         alignItems: "center",
+    },
+    flexContainer:{
+        position: 'absolute',
+        height: '100%',
+        width: '100%'
     }
+    
  })

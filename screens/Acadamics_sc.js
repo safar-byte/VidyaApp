79f2607a9131d70e774579aa7bd262706f779ffc
Safar <safarmuhmmed@gmail.com>
2022-12-30 18:00:21 +0530

    added the site link

--- screens/Acadamics_sc.js
+++ screens/Acadamics_sc.js
@@ -2,19 +2,83 @@ import React from "react";
 import { Text, View ,StyleSheet} from "react-native";
 
 export default function Acadamics_sc(){
+        
+const webViewRef = useRef(null);
+const onAndroidBackPress = () => {
+  if (webViewRef.current) {
+    webViewRef.current.goBack();
+    return true; // prevent default behavior (exit app)
+  }
+  return false;
+};
+
+
+
+useEffect(() => {
+    if (Platform.OS === 'android') {
+        BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+        return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    };
+}
+}, []);
+const [isInternetReachable, setisInternetReachable] = useState(false);
+useEffect(()=>{
+    const unsubscribe = NetInfo.addEventListener(state => {
+        setisInternetReachable(state.isInternetReachable)
+    });
+    return () => unsubscribe();
+    
+},[])
+
+//remove all warn
+LogBox.ignoreAllLogs();
+
+//trying to remove header
+const runFirst = `
+      true; 
+    `;
     return(
-        <View style={styles.cont}>
-            <Text>Acadamics</Text>
-        </View>
+        <>
+      
+        <View style =  {{flex: 1}}>
+        {isInternetReachable ?   
+        <WebView
+        ref={webViewRef}
+        style={styles.container}
+        source={{ uri: 'https://vidyatcklmr.ac.in/departments.php'}}
+        startInLoadingState={true}
+        injectedJavaScript={runFirst}
+        renderError={()=>(<SomethingWent/>)}
+        renderLoading={() => (
+          <ActivityIndicator
+          color="black"
+          size="large"
+          style={styles.flexContainer}
+          />
+          )}
+          />:<OfflineNotice/>
+          
+        }
+      
+</View>
+      </>
     );
 }
 const styles = StyleSheet.create({
-    cont: {
+    container: {
         flex: 1,
         backgroundColor: '#E5BA73',
         justifyContent: "center",
         alignItems: "center",
+    },
+    flexContainer:{
+        position: 'absolute',
+        height: '100%',
+        width: '100%'
     }
+    
  })
+
 
  

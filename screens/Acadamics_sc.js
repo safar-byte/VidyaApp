d3f8ce080f80c9b92834d4418e6a0081758a403d
Safar <safarmuhmmed@gmail.com>
2022-12-30 12:20:09 +0530

    adcamdic basic

--- screens/Acadamics_sc.js
+++ screens/Acadamics_sc.js
@@ -1,20 +1,95 @@
-import React from "react";
-import { Text, View ,StyleSheet} from "react-native";
-
+import React,{useEffect,useRef,useState}  from "react";
+import { Text, View ,StyleSheet,ActivityIndicator,BackHandler,Platform,LogBox} from "react-native";
+import { WebView } from 'react-native-webview';
+import OfflineNotice from "../components/OfflineNotice";
+import NetInfo from "@react-native-community/netinfo";
+import SomethingWent from "../components/SomethingWent";
 export default function Acadamics_sc(){
-    return(
-        <View style={styles.cont}>
-            <Text>Acadamics</Text>
-        </View>
-    );
-}
-const styles = StyleSheet.create({
-    cont: {
-        flex: 1,
-        backgroundColor: '#E5BA73',
-        justifyContent: "center",
-        alignItems: "center",
+    
+    const webViewRef = useRef(null);
+    const onAndroidBackPress = () => {
+      if (webViewRef.current) {
+        webViewRef.current.goBack();
+        return true; // prevent default behavior (exit app)
+      }
+      return false;
+    };
+    
+    
+    
+    useEffect(() => {
+        if (Platform.OS === 'android') {
+            BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+            return () => {
+          BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+        };
     }
- })
+    }, []);
+    const [isInternetReachable, setisInternetReachable] = useState(false);
+    useEffect(()=>{
+        const unsubscribe = NetInfo.addEventListener(state => {
+            setisInternetReachable(state.isInternetReachable)
+        });
+        return () => unsubscribe();
+        
+    },[])
+    
+    //remove all warn
+    LogBox.ignoreAllLogs();
+    
+    
+    //trying to remove header
+    const runFirst = `
+ 
+    let admisidrm = document.querySelector("div#slide-panel")
+    admisidrm.style.display = "none"
+    let ytsubrm = document.querySelector('.text-center')
+    ytsubrm.style.display = "none"
+    let tolirm = document.querySelector('.insti wow fadeInLeft  ')
+    tolirm.style.display = "none"
 
+          true; 
+        `;
+    return(
+        <>
+          
+            <View style =  {{flex: 1}}>
+            {isInternetReachable ?   
+            <WebView
+            ref={webViewRef}
+            style={styles.container}
+            source={{ uri: 'https://vidyatcklmr.ac.in/departments.php'}}
+            startInLoadingState={true}
+            injectedJavaScript={runFirst}
+            renderError={()=>(<SomethingWent/>)}
+            renderLoading={() => (
+              <ActivityIndicator
+              color="black"
+              size="large"
+              style={styles.flexContainer}
+              />
+              )}
+              />:<OfflineNotice/>
+              
+            }
+          
+    </View>
+          </>
+        );
+    }
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#E5BA73',
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        flexContainer:{
+            position: 'absolute',
+            height: '100%',
+            width: '100%'
+        }
+        
+     })
+    
  

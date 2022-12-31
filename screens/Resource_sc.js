cce9e406962d88d3d4ace6e63315cc16f51573d6
glitch-head <118680463+glitch-head@users.noreply.github.com>
2022-12-31 22:38:43 +0530

    resource animation err

--- screens/Resource_sc.js
+++ screens/Resource_sc.js
@@ -6,46 +6,71 @@ import NetInfo from "@react-native-community/netinfo";
 import SomethingWent from "../components/SomethingWent"; 
 
 export default function Resource_sc(){
-    const webViewRef = useRef(null);
-const onAndroidBackPress = () => {
-  if (webViewRef.current) {
-    webViewRef.current.goBack();
-    return true; // prevent default behavior (exit app)
-  }
-  return false;
-};
+  const webViewRef = useRef();
 
+  useEffect(() => {
+      if (Platform.OS === 'android') {
+        BackHandler.addEventListener('hardwareBackPress', HandleBackPressed);
 
-
-useEffect(() => {
-    if (Platform.OS === 'android') {
-        BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
         return () => {
-      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
-    };
-}
-}, []);
-const [isInternetReachable, setisInternetReachable] = useState(false);
-useEffect(()=>{
-    const unsubscribe = NetInfo.addEventListener(state => {
-        setisInternetReachable(state.isInternetReachable)
-    });
-    return () => unsubscribe();
-    
-},[])
+           BackHandler.removeEventListener('hardwareBackPress', HandleBackPressed);
+        }
+      }
+    }, []); // INITIALIZE ONLY ONCE
 
+    const HandleBackPressed = () => {
+        if (webViewRef.current.canGoBack) {
+          webViewRef.current.goBack();
+            return true; // PREVENT DEFAULT BEHAVIOUR (EXITING THE APP)
+        }
+        return false;
+    }
+    const [isInternetReachable, setisInternetReachable] = useState(false);
+    useEffect(() => {
+        const unsubscribe = NetInfo.addEventListener(state => {
+            setisInternetReachable(state.isInternetReachable)
+        });
+        return () => unsubscribe();
+
+    }, [])
 //remove all warn
 LogBox.ignoreAllLogs();
- return(
-  <>      
-           <View style =  {{flex: 1}}>
+
+//trying to remove header
+const injectJS = () => {
+
+  webViewRef.current.injectJavaScript(`
+
+   document.querySelector("body > section.cont_bg > div:nth-child(1) > div > div > div.col-lg-3.col-md-3.col-sm-12.col-xs-12.cont_L_bordr").remove();
+  document.querySelector("body > section.hed > div.menu.wow.fadeInRight > div > div > div.col-lg-10.col-md-12.col-sm-12.col-xs-12").remove();
+  document.querySelector("#slide-panel").remove();
+  document.querySelector("body > section.hed > div.insti.wow.fadeInLeft > div > div > div.col-lg-4.col-md-5.col-sm-12.col-xs-12").remove();
+  //document.querySelector("body > section.banner.wow.fadeIn").remove();
+  document.querySelector("body > section.quk_lnk.wow.fadeInUp").remove();
+  document.querySelector("body > section.cont_bg > div:nth-child(3)").remove();
+  document.querySelector("body > section.cont_bg > div:nth-child(4) > div").remove();
+  //document.querySelector("body > section.cont_bg > div:nth-child(5)").remove();
+  //document.querySelector("body > section.cont_bg > div:nth-child(5) > div > div > div").remove();
+  "window.scrollTo(100,100);"
+    ; 
+    `
+    ,
+        );
+
+    };
+
+    return(
+        <>
+      
+        <View style =  {{flex: 1}}>
         {isInternetReachable ?   
         <WebView
         ref={webViewRef}
+        onLoad={injectJS}
+        animationType='none'
         style={styles.container}
-        source={{ uri: 'https://www.vidyatcklmr.ac.in/page.php?page=92 '}}
+        source={{ uri: 'https://www.vidyatcklmr.ac.in/page.php?page=92'}}
         startInLoadingState={true}
-       r
         renderError={()=>(<SomethingWent/>)}
         renderLoading={() => (
           <ActivityIndicator
@@ -65,7 +90,7 @@ LogBox.ignoreAllLogs();
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#E5BA73',
+        backgroundColor: '#fff',
         justifyContent: "center",
         alignItems: "center",
     },
@@ -76,3 +101,6 @@ const styles = StyleSheet.create({
     }
     
  })
+
+
+ 

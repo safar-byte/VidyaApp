eadb53e055a7d537788dab04afc0d77f1c0bd240
Safar <safarmuhmmed@gmail.com>
2023-01-05 18:27:29 +0530

    qucik fix in resource

--- screens/Resource_sc.js
+++ screens/Resource_sc.js
@@ -1,12 +1,12 @@
-import React,{useEffect,useRef,useState}  from "react";
-import { Text, View ,StyleSheet,Modal,ActivityIndicator,BackHandler,Platform,LogBox} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Text, View, StyleSheet, Modal, ActivityIndicator, BackHandler, Platform, LogBox } from "react-native";
 import { WebView } from 'react-native-webview';
 import OfflineNotice from "../components/OfflineNotice";
 import NetInfo from "@react-native-community/netinfo";
-import SomethingWent from "../components/SomethingWent"; 
+import SomethingWent from "../components/SomethingWent";
 
-export default function Resource_sc(){
-  const webViewRef = useRef();
+export default function Resource_sc() {
+  const webViewRef = useRef(null);
   const [canGoBack, setCanGoBack] = useState(false);
   const [visible, setVisible] = useState(true);
 
@@ -31,21 +31,22 @@ export default function Resource_sc(){
 
 
 
-    const [isInternetReachable, setisInternetReachable] = useState(false);
-    useEffect(() => {
-        const unsubscribe = NetInfo.addEventListener(state => {
-            setisInternetReachable(state.isInternetReachable)
-        });
-        return () => unsubscribe();
+  const [isInternetReachable, setisInternetReachable] = useState(false);
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setisInternetReachable(state.isInternetReachable);
+      setVisible(false);
+    });
+    return () => unsubscribe();
 
-    }, [])
-//remove all warn
-LogBox.ignoreAllLogs();
+  }, [])
+  //remove all warn
+  LogBox.ignoreAllLogs();
 
-//trying to remove header
-const injectJS = () => {
+  //trying to remove header
+  const injectJS = () => {
 
-  webViewRef.current.injectJavaScript(`
+    webViewRef.current.injectJavaScript(`
 
   document.querySelector("body > section.hed").remove();
   document.querySelector("body > section.quk_lnk.wow.fadeInUp").remove();
@@ -55,80 +56,80 @@ const injectJS = () => {
   window.ReactNativeWebView.postMessage("Main_page");
     ; 
     `
-    ,
-        );
+      ,
+    );
 
-    };
-    const onNavigationStateChange = (navState) => {
-      setCanGoBack(navState.canGoBack);
-      setVisible(true);
-      webViewRef.current.injectJavaScript(
+  };
+  const onNavigationStateChange = (navState) => {
+    setCanGoBack(navState.canGoBack);
+    setVisible(true);
+    webViewRef.current.injectJavaScript(
 
-        `document.querySelector("#header-text-nav-container").remove();
+      `document.querySelector("#header-text-nav-container").remove();
         document.querySelector("#secondary").remove();
         document.querySelector("#colophon").remove();
+        window.ReactNativeWebView.postMessage("page_2");
          ;`,
-      )
-    
-    };
-    const onMessage = (event) => {
+    )
 
-      setVisible(false);
-  
-  
-    }
+  };
+  const onMessage = (event) => {
 
-    return(
-        <>
-      
-        <View style =  {{flex: 1}}>
-        {isInternetReachable ?   
+    setVisible(false);
+
+
+  }
+
+  return (
+
+
+    <View style={{ flex: 1 }}>
+      {isInternetReachable ?
         <WebView
-        ref={webViewRef}
-        onLoad={injectJS}
-        onMessage={onMessage}
-        onNavigationStateChange={onNavigationStateChange}
-        style={styles.container}
-        source={{ uri: 'https://www.vidyatcklmr.ac.in/page.php?page=92'}}
-        startInLoadingState={true}
-        renderError={()=>(<SomethingWent/>)}
-        renderLoading={() => (
-          <ActivityIndicator
-          color="black"
-          size="large"
-          style={styles.flexContainer}
-          />
+          ref={webViewRef}
+          onLoad={injectJS}
+          onMessage={onMessage}
+          onNavigationStateChange={onNavigationStateChange}
+          style={styles.container}
+          source={{ uri: 'https://www.vidyatcklmr.ac.in/page.php?page=92' }}
+          startInLoadingState={true}
+          renderError={() => (<SomethingWent />)}
+          renderLoading={() => (
+            <ActivityIndicator
+              color="black"
+              size="large"
+              style={styles.flexContainer}
+            />
           )}
-          />:<OfflineNotice/>
-          
-        }
-        <Modal visible={visible}>
+        /> : <OfflineNotice />
+
+      }
+      <Modal visible={visible}>
         <ActivityIndicator
           color="black"
           size="large"
           style={styles.flexContainer}
         />
       </Modal>
-      
-</View>
 
-      </>
-    );
+    </View>
+
+
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    flexContainer:{
-        position: 'absolute',
-        height: '100%',
-        width: '100%'
-    }
-    
- })
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  flexContainer: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%'
+  }
+
+})
 
 
- 
